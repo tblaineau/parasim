eb9b342ade8dbc3ec990490ec01e77290474d071
tristanblaineau <tristan.blaineau@orange.fr>
2019-12-02 18:36:13 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='merger',
+	version='0.0a',
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=[
+		'numpy',
+		'merger',
+		'pandas',
+		'iminuit',
+		'scipy',
+		'numba']
+)
