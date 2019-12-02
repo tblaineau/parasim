e959b7cc92e988a140ee7c080ad6bdcf1b3fd885
tristanblaineau <tristan.blaineau@orange.fr>
2019-12-02 18:42:25 +0100

    corrected setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='merger',
+setup(name='parallax_simulator_pipeline',
 	version='0.0a',
 	packages=find_packages(),
 	include_package_data=True,
