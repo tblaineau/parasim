2d808b1f9682f7a889e88576438379223df884f1
tristanblaineau <tristan.blaineau@orange.fr>
2019-12-03 12:04:24 +0100

    added script

---
+++ parallax_simulator_pipeline/psim.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#$-N prod_parallax_distance_u02_minmax_run2
+#$-P P_eros
+#$-t 1-1:1
+#$-q long
+#$-o $HOME/work/parallax_jobs/run2/logs_minmax
+#$-e $HOME/work/parallax_jobs/run2/logs_minmax
+
+SGE_TASK_ID=1
+
+#"$HOME/work/merger/merger/parallax_estimator/se_script.py"
+python3 "se_script.py" --name "$1_$SGE_TASK_ID.pkl" --parameter_file "$2" \
+--nb_line_pmsfile "$3" --nb_files "$4" --nb_jobs 100  --current_job "$SGE_TASK_ID"
+
+for var in *.pkl
+do
+        echo "$var"
+        cp "$var" "$HOME/work/parallax_jobs/run2/minmax"
+done
--- parallax_simulator_pipeline/se_script.py
+++ parallax_simulator_pipeline/se_script.py
@@ -1,4 +1,3 @@
-
 import argparse
 import os
 import numpy as np
@@ -7,30 +6,41 @@ from parallax_simulator_pipeline.similarity_estimator import count_peaks, comput
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--name', '-n', type=str, required=True, help="output file name")
-	parser.add_argument('--parameter_file', '-pf', type=str, required=True, help="template name of parameter file, ex : parameters1M_")
-	parser.add_argument('--nb_samples_job', '-nsj', type=int, required=True, help="Number of light curve to process in one job.")
-	parser.add_argument('--current_job', '-cj', type=int, required=True, help="job number")
+	parser.add_argument('--parameter_file', type=str, required=True,
+						help="template name of parameter file, ex : parameters1M_")
+	parser.add_argument('--nb_line_pmsfile', type=int, required=True,
+						help="Number of light curve per parameters file.")
+	parser.add_argument('--nb_files', type=int, required=True, help="Number of parameters file.")
+	parser.add_argument('--nb_jobs', type=int, required=False, default=100, help="Number of jobs.")
+	parser.add_argument('--current_job', type=int, required=True, help="current job number")
 
 	args = parser.parse_args()
 
-	nb_samples_job = args.nb_samples_job
+	nb_line_pmsfile = args.nb_line_pmsfile
+	nb_files = args.nb_files
+	nb_jobs = args.nb_jobs
 	current_job = args.current_job
 
-	assert nb_samples_job > 0, 'Invalid number of samples per job.'
+	nb_samples_job = int(nb_line_pmsfile * nb_files / nb_jobs)
+	factor = int(nb_line_pmsfile / nb_samples_job)
+
+	print(nb_line_pmsfile, nb_files, nb_jobs, nb_samples_job)
+
+	assert nb_line_pmsfile > 0, 'Invalid number of line per file.'
 	assert current_job > 0, 'Invalid current job number.'
+	assert nb_samples_job - (nb_line_pmsfile * nb_files / nb_jobs) == 0, f'nb_samples_job is not an integer : {nb_line_pmsfile * nb_files / nb_jobs}'
+	assert int(nb_line_pmsfile / nb_samples_job) - (nb_line_pmsfile / nb_samples_job) == 0, f'factor is not an integer :{nb_line_pmsfile / nb_samples_job}'
 
-	params_file_idx = (current_job-1) // 5
-	params_line_idx = (current_job-1) % 5
-
-	print(f"File : {params_file_idx}")
-	print(f"line_start : {params_line_idx}")
+	params_file_idx = (current_job - 1) // factor
+	params_line_idx = (current_job - 1) % factor
 
 	start = params_line_idx * nb_samples_job
 	end = (params_line_idx + 1) * nb_samples_job
 
 	output_name = args.name
-	parameter_file = args.parameter_file+'_'+str(params_file_idx)+'.npy'
+	parameter_file = args.parameter_file + '_' + str(params_file_idx) + '.npy'
 
-	assert os.path.isfile(parameter_file), 'Parameter file does not exist : '+parameter_file
+	assert os.path.isfile(parameter_file), 'Parameter file does not exist : ' + parameter_file
 
-	compute_distances(output_name, distance=count_peaks, parameter_list=np.load(parameter_file, allow_pickle=True), start=start, end=end)
+	compute_distances(output_name, distance=count_peaks, parameter_list=np.load(parameter_file, allow_pickle=True),
+					  start=start, end=end)
