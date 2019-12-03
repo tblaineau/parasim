270632a6541480c2cde0c2e94ac4c353f062e865
tristanblaineau <tristan.blaineau@orange.fr>
2019-12-03 13:50:04 +0100

    ready

--- parallax_simulator_pipeline/psim.sh
+++ parallax_simulator_pipeline/psim.sh
@@ -7,8 +7,6 @@
 #$-o $HOME/work/parallax_jobs/run2/logs_minmax
 #$-e $HOME/work/parallax_jobs/run2/logs_minmax
 
-SGE_TASK_ID=1
-
 #"$HOME/work/merger/merger/parallax_estimator/se_script.py"
 python3 "se_script.py" --name "$1_$SGE_TASK_ID.pkl" --parameter_file "$2" \
 --nb_line_pmsfile "$3" --nb_files "$4" --nb_jobs 100  --current_job "$SGE_TASK_ID"
