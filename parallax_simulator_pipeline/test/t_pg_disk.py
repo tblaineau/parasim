6c5ac8cae34770781b93cb1ff940afcc667b477d
tristanblaineau <tristan.blaineau@orange.fr>
2020-01-23 15:41:04 +0100

    thetas check

--- parallax_simulator_pipeline/test/t_pg_disk.py
+++ parallax_simulator_pipeline/test/t_pg_disk.py
@@ -9,5 +9,17 @@ st1 = time.time()
 s_thick = pgd.metropolis_hastings(pgd.pdf_xvs_disk, pgd.randomize_gauss_total_hardcoded, 1000000, np.array([0.9, 10., 10., 10.]), (56.1, 46.1, 35.1, 35, 1000., 35000.))
 print(time.time()-st1)
 
-print(s_thick)
-print(pgd.compute_thetas(s_thick))
+#print(s_thick)
+thetas = pgd.compute_thetas(s_thick)
+print(thetas)
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(10, 10))
+ax = plt.subplot(111, projection='polar')
+i = pgd.compute_i()
+ax.set_theta_zero_location("E", offset=np.arctan2(i[2], i[1])*180./np.pi)
+ax.hist(thetas, bins=100)
+ax.grid(True)
+x = np.linspace(0, 1, 10)
+plt.show()
